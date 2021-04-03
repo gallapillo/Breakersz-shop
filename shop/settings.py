2136a326ae1ee00f05c1173758e784e20a57cc41
gallapillo <58660325+gallapillo@users.noreply.github.com>
2021-04-03 20:06:56 +0700

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lm@6f4crv9h9ioa-0deh*pe%*1u6#__tqpzp2g1qo2kw2iyzhn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['breaksz-shop.herokuapp.com']#https://breaksz-shop.herokuapp.com/
 
